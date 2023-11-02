cd1a2a26a7e7b9bec4b523908a597d32495055d5
Abbas Aliyev <alievabbas1@gmail.com>
2023-11-03 02:11:21 +0400

    File list functionality

--- src/routes/fileRoutes.ts
+++ src/routes/fileRoutes.ts
@@ -5,6 +5,7 @@ import * as uuid from 'uuid';
 import dataSource from 'db/app-data-source';
 import { File } from 'db/entities/file.entity';
 import passport from 'passport';
+import { celebrate, Joi } from 'celebrate';
 
 
 const folder = path.join(process.cwd(), 'uploads');
@@ -43,6 +44,32 @@ fileRouter.post('/upload', passport.authenticate('jwt'), upload.single('file'),
     }
 
     res.send({ msg: 'OK', data: newFile, error: null });
+});
+
+fileRouter.get('/list', passport.authenticate('jwt'), celebrate(
+  {
+    query: {
+      page: Joi.number().min(1).default(1),
+      limit: Joi.number().min(1).default(10),
+    }
+  }
+), async (req, res) => {
+  const page = +req.query.page;
+  const limit = +req.query.limit;
+
+  const fileRepo = dataSource.getRepository(File);
+
+  let files: File[];
+  try {
+    files = await fileRepo.find({
+      skip: (page - 1) * limit,
+      take: limit,
+    });
+  } catch (error) {
+    res.status(500).send({ msg: 'Internal Server Error', status: 500, data: null, error: error.message });
+  }
+
+  res.send({ msg: 'OK', data: files, error: null });
 });
 
 
